3abf377a494682fe8e8f84ca8927416e506c7fe9
Mostafa <31211@ma-web.nl>
2022-11-24 10:28:10 +0100

    les1

---
+++ les1/app.js
@@ -0,0 +1,25 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello worled!");
+        let appNaam = "Mostafa"
+        let versienummer = (.9)
+        let versiedatum = "24/11/2022"
+        let autheur = "Mostafa"
+        let copyright = "string"
+        let distributeur = "string"
+        let darkmode = (true)
+
+        console.log(appNaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
+    }
+}
+
+let app = new App();
+app.runApplication();
