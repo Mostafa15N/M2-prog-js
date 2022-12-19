00e2da196afa6ac6ade0960feca52c87b653cb66
Mostafa <31211@ma-web.nl>
2022-12-19 10:34:14 +0100

    ifelse

--- Loops en Arrays/Canvas 1 HTML/app.js
+++ Loops en Arrays/Canvas 1 HTML/app.js
@@ -1,22 +1,47 @@
 class App
 {
     runApplication()
+   {
+    let title = document.getElementById("newstitle");
+    let randomGetal = Math.random()
+    
+    if (randomGetal== 0)
     {
-        console.log("hello world");
-        let arr = ["Nancy Ajram", "Assala", "Kadim", "Hussain Al Jassmi"];
-        console.log(arr);
-
-        let headers = document.getElementsByClassName("bandName")
-
-        headers[0].innerHTML = arr[0];
-        headers[1].innerHTML = arr[1];
-        headers[2].innerHTML = arr[2];
-        headers[3].innerHTML = arr[3];
-
-        for(let i= 0; i < headers.length; i++){
-            headers[i].innerHTML = [i];
-        }
+      title.style.backgroundColor = "#FFFF00";
     }
+    else if (randomGetal< 0.2)
+    {
+      title.style.backgroundColor = "#DDA0DD";
+    }    
+    else if (randomGetal < 0.6)
+    {
+       title.style.backgroundColor = "#7CFC00";
+    }    
+    else if (randomGetal < 0.8)
+    {
+       title.style.backgroundColor = "#00FFFF";
+    }
+    else{
+       title.style.background = "#7B68EE";
+    }
+    {let appnaam = "Discord";
+    let versienummer = 0.9;
+    let versiedatum = "12-09-2022";
+    let autheur = "Joseph";
+    let copyright = "That includes such things as manuscripts, books, phonorecords, cards,"
+      "disks, film, and tapes are among some of the written materials that are considered protected under copyright law.";
+    let distributeur = "Amazon"
+    let darkmode = true
+    console.log("hello world!")
+    console.log(appnaam)
+    console.log(versienummer)
+    console.log(versiedatum)
+    console.log(autheur)
+    console.log(copyright)
+    console.log(distributeur)
+    console.log(darkmode)
+   }
+   }
 }
 
 let app = new App();
